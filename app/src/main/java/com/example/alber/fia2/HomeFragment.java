e01574d2efb313d4888ee3c38dacdaf463f136ee
alberto oviedo <albertooviedo83@gmail.com>
2018-11-20 22:04:57 -0300

    Funcionando Modulo home
    completado el modulo Home

--- app/src/main/java/com/example/alber/fia2/HomeFragment.java
+++ app/src/main/java/com/example/alber/fia2/HomeFragment.java
@@ -3,8 +3,6 @@ package com.example.alber.fia2;
 
 import android.content.Context;
 
-import android.content.Intent;
-
 import android.content.SharedPreferences;
 
 import android.os.Bundle;
@@ -14,7 +12,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -24,16 +21,11 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import static android.content.Context.MODE_PRIVATE;
-
 
 
 /**
@@ -43,11 +35,13 @@ public class HomeFragment extends Fragment {
 
 
     public FloatingActionButton fab;
+    public View rootView;
     public FirebaseFirestore db = FirebaseFirestore.getInstance();
-    public Task<QuerySnapshot> deuda;
     public ArrayList<String> lista_rut;
+    public ArrayList<String> lista_suma;
     public ArrayList<String> lista_nombre;
     public ArrayList<String> lista_monto;
+    public ArrayList<String> lista_monto_pago;
     private SharedPreferences.Editor editor;
 
 
@@ -65,7 +59,9 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        final View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        ListView listView = rootView.findViewById(R.id.home_list_view);
+        initData(listView);
         fab = (FloatingActionButton) rootView.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,48 +70,46 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        /*db.collection("Deudas").whereEqualTo("id_negocio", "hWfyjMYmx4c40GxXchUE").get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                         lista_rut = new ArrayList<>();
-                         lista_nombre = new ArrayList<>();
-                         lista_monto = new ArrayList<>();
-                        for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()){
-                            lista_rut.add((String) queryDocumentSnapshot.get("rut"));
-                            lista_nombre.add((String) queryDocumentSnapshot.get("nombre"));
-                            lista_monto.add((String) queryDocumentSnapshot.get("monto"));
-                        }
-                    }
-                });
-        Toast.makeText(rootView.getContext(), (CharSequence) lista_monto, Toast.LENGTH_SHORT).show();
-        db.collection("pagos").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        return rootView;
+    }
+    public void initData(final ListView listview){
+        lista_monto = new ArrayList<>();
+        lista_monto_pago = new ArrayList<>();
+        lista_nombre = new ArrayList<>();
+        lista_rut = new ArrayList<>();
+        lista_suma = new ArrayList<>();
+        db.collection("Deudas").whereEqualTo("id_negocio","hWfyjMYmx4c40GxXchUE")
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                ListView lista = rootView.findViewById(R.id.home_list_view);
-                ArrayList<String> coleccion = new ArrayList<>();
-                Integer contador = 0;
-                for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()){
-                    if (lista_rut.get(contador) == queryDocumentSnapshot.get("rut")){
-                        int monto1 = Integer.parseInt((String) queryDocumentSnapshot.get("monto"));
-                        int monto2 = Integer.parseInt(lista_monto.get(contador));
-                        int suma = monto1 - monto2;
-                        coleccion.add((String.format("%s: %s", lista_nombre.get(contador), suma)));
-
+              for (QueryDocumentSnapshot queryDocumentSnapshot: task.getResult()) {
+                  lista_rut.add((String) queryDocumentSnapshot.get("rut"));
+                  lista_monto.add((String) queryDocumentSnapshot.get("monto"));
+                  lista_nombre.add((String) queryDocumentSnapshot.get("nombre"));
+              }
+                db.collection("Pagos").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        for (QueryDocumentSnapshot queryDocumentSnapshot: task.getResult()){
+                            if (lista_rut.contains(queryDocumentSnapshot.get("rut"))){
+                                lista_monto_pago.add((String) queryDocumentSnapshot.get("monto"));
+                            }
+                            else{
+                            }
+                        }
+                        for (int i = 0; i != lista_monto_pago.size(); i++) {
+                            int suma = (Integer.parseInt(lista_monto_pago.get(i)) - Integer.parseInt(lista_monto.get(i)));
+                            lista_suma.add(lista_nombre.get(i)+ ": " + Integer.toString(suma)  );
+                        }
+                        ArrayAdapter adapter = new ArrayAdapter(rootView.getContext(),android.R.layout.simple_list_item_1,lista_suma);
+                        listview.setAdapter(adapter);
                     }
-                    else{
-                        continue;
-                    }
-                }
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(rootView.getContext(),android.R.layout.simple_list_item_1,coleccion);
-                lista.setAdapter(adapter);
+                });
             }
-        });*/
+        });
 
-        return rootView;
 
     }
-
 
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
         inflater.inflate(R.menu.menu_main,menu);
